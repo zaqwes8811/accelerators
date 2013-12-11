5e5cfa39c66edd0c019c9612af6d312095413898
zaqwes8811 <igor.a.lugansky@gmail.com>
2013-12-11 10:57:29 +0400

    save and push

--- cuda/square.cu
+++ cuda/square.cu
@@ -2,7 +2,35 @@
 // C
 #include <stdio.h>
 
-int main(int argc, char ** argv) {
+__global__ void square(
+    float* d_out,
+    float* d_in) 
+  {
+  int idx = threadIdx.x;
+  float f = d_in[idx];
+  d_out[idx] = f * f;
+}
 
+int main(int argc, char ** argv) {
+  const int ARRAY_SIZE = 64;
+  const int ARRAY_BYTES = ARRAY_SIZE * sizeof(float);
+  
+  // generate the input array on the host
+  float h_in[ARRAY_SIZE];
+  for (int i = 0; i < ARRAY_SIZE; i++) {
+    h_in[i] = float(i);
+  }
+  float h_out[ARRAY_SIZE];
+  
+  // declare GPU memory pointers
+  float * d_in;
+  float * d_out;
+  
+  
+  // copy back the result array to the CPU
+  cudaMemcpy(d_in, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+  
+  
+  
   return 0;
 }
