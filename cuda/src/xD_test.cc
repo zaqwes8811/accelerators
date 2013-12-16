6bb8f034b4be4c9c77b7ed949f196f274aa0d8a0
zaqwes8811 <igor.a.lugansky@gmail.com>
2013-12-16 19:29:17 +0400

    save and push

--- cuda/src/xD_test.cc
+++ cuda/src/xD_test.cc
@@ -4,5 +4,25 @@
 #include <gtest/gtest.h>
 
 TEST(xD, Base) {
+  // Filter
+  float const filter2D[3][3] = {
+    {1, 1, 1},
+    {1, 1, 1},
+    {1, 1, 1}
+  };
+  float filter1D[sizeof filter2D];
 
+  // Image
+  const int ROWS = 7;
+  const int COLUMNS = 9;
+  float h_image2D[ROWS][COLUMNS];
+  float h_image1D[sizeof h_image2D];
+  for (int r = 0; r < ROWS; r++) {
+    for (int c = 0; c < COLUMNS; c++) {
+      h_image2D[r][c] = c;
+      h_image1D[r * COLUMNS + c] = c;
+    }
+  }
+
+  EXPECT_EQ(h_image2D[4][3], h_image1D[4 * COLUMNS + 3]);
 }
