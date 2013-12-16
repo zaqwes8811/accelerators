027997e163257f41c5d3e562af4bec20c899580b
zaqwes8811 <igor.a.lugansky@gmail.com>
2013-12-16 19:40:38 +0400

    save and push

--- cuda/src/xD_test.cc
+++ cuda/src/xD_test.cc
@@ -7,18 +7,24 @@
 typedef unsigned char uint8_t;
 
 TEST(xD, Base) {
+  const size_t ROWS = 7;
+  const size_t COLUMNS = 9;
+  const size_t FILTER_WIDTH = 3;
   // Filter
-  float const filter2D[3][3] = {
+  float const filter2D[FILTER_WIDTH][FILTER_WIDTH] = {
     {1, 1, 1},
     {1, 1, 1},
     {1, 1, 1}
   };
   float filter1D[sizeof filter2D];
 
-  // Image
-  const size_t ROWS = 7;
-  const size_t COLUMNS = 9;
+  for (int r = 0; r < FILTER_WIDTH; r++) {
+    for (int c = 0; c < FILTER_WIDTH; c++) {
+      filter1D[r * COLUMNS + c] = filter2D[r][c];
+    }
+  }
 
+  // Image
   uint8_t h_image2D[ROWS][COLUMNS];
   uint8_t h_InImage1D[sizeof h_image2D];
 
@@ -34,5 +40,9 @@ TEST(xD, Base) {
   EXPECT_EQ(h_image2D[4][3], h_InImage1D[4 * COLUMNS + 3]);
 
   // —читаем через ref-функцию
-
+  channelConvolutionRefa(
+    h_InImage1D,
+    h_Outimage1D,
+    ROWS, COLUMNS,
+    filter1D, FILTER_WIDTH);
 }
