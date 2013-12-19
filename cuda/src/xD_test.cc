02ec38874b53ee7c0a28115fd43c2f50bd97ee67
zaqwes8811 <igor.a.lugansky@gmail.com>
2013-12-19 20:09:19 +0400

    Fix errors and run simple code.

--- cuda/src/xD_test.cc
+++ cuda/src/xD_test.cc
@@ -3,6 +3,7 @@
 #include "cs344/summary/reference_calc.h"
 
 #include "cs344/HW2/hw2_kernels_cu.h"
+#include "cs344/reuse/utils.h"
 
 typedef unsigned char uint8_t;
 
@@ -16,11 +17,11 @@ TEST(xD, Base) {
     {1, 1, 1},
     {1, 1, 1}
   };
-  float filter1D[sizeof filter2D];
+  float h_filter1D[sizeof filter2D];
 
   for (int r = 0; r < FILTER_WIDTH; r++) {
     for (int c = 0; c < FILTER_WIDTH; c++) {
-      filter1D[r * COLUMNS + c] = filter2D[r][c];
+      h_filter1D[r * COLUMNS + c] = filter2D[r][c];
     }
   }
 
@@ -39,15 +40,45 @@ TEST(xD, Base) {
 
   EXPECT_EQ(h_image2D[4][3], h_InImage1D[4 * COLUMNS + 3]);
 
+  /// /// ///
+
   // —читаем через ref-функцию
   channelConvolutionRefa(
     h_InImage1D,
     h_Outimage1D,
     ROWS, COLUMNS,
-    filter1D, FILTER_WIDTH);
+    h_filter1D, FILTER_WIDTH);
 
   // CUDA
+  uint8_t* d_InImage1D;
+  uint8_t* d_OutImage1D;
+  float* d_filter1D;
+  
+  // Allocate on GPU side
+  const int numPixels = ROWS * COLUMNS;
+  checkCudaErrors(cudaMalloc((void**)&d_InImage1D, sizeof(unsigned char) * numPixels));
+  checkCudaErrors(
+    cudaMemcpy(d_InImage1D, h_InImage1D, sizeof(unsigned char) * numPixels, cudaMemcpyHostToDevice));
+
+  checkCudaErrors(cudaMalloc((void**)&d_OutImage1D, sizeof(unsigned char) * numPixels));
+  checkCudaErrors(cudaMemset(d_OutImage1D, 0, numPixels * sizeof(unsigned char)));
+
+  // Put filter
+  checkCudaErrors(cudaMalloc((void**)&d_filter1D, sizeof(unsigned char) * FILTER_WIDTH));
+  checkCudaErrors(
+    cudaMemcpy(d_filter1D, h_filter1D, sizeof(unsigned char) * FILTER_WIDTH, cudaMemcpyHostToDevice));
+
+
   run_test_blur(
-      h_InImage1D, h_Outimage1D, ROWS, COLUMNS,
-      filter1D, FILTER_WIDTH);
+      d_InImage1D, d_OutImage1D, ROWS, COLUMNS,
+      d_filter1D, FILTER_WIDTH);
+
+  checkCudaErrors(
+    cudaMemcpy(h_Outimage1D, d_OutImage1D, sizeof(unsigned char) * numPixels, cudaMemcpyDeviceToHost));
+
+
+
+  checkCudaErrors(cudaFree(d_filter1D));
+  checkCudaErrors(cudaFree(d_OutImage1D));
+  checkCudaErrors(cudaFree(d_InImage1D));
 }
