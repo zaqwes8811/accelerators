30a8cabc710da721e2d0c237c3b7f4ab99400d6d
zaqwes <zaqwes@localhost.localdomain>
2014-04-17 13:33:40 -0400

    Run HW1 test.

--- cuda/slns/Debug/sources.mk
+++ cuda/slns/Debug/sources.mk
@@ -26,7 +26,9 @@ C_UPPER_DEPS :=
 # Every subdirectory with source files must be described here
 SUBDIRS := \
 third_party \
-src/work/hided \
+src \
+src/cuda \
 src/cs344/summary \
 src/cs344/reuse \
+src/cc \
 
