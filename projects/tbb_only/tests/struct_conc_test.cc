3350f3f367be578ee72e4dd5d85def060cf9d5d7
zaqwes <igor.a.lugansky@gmail.com>
2015-02-27 13:37:09 +0300

    save

--- projects/tbb_only/tests/struct_conc_test.cc
+++ projects/tbb_only/tests/struct_conc_test.cc
@@ -1,3 +1,6 @@
+#include <gtest/gtest.h>
+#include <tbb/parallel_for.h>
+
 // design patt -> (!)alg strategy patt -> impl patt
 
 // best strategy for scalable parallelism is data parallelism
@@ -10,8 +13,8 @@
 // task - potential parallel work - soft to hard threads - preemptive - scheduler task to soft th - cooperative
 // less context switches - threads have mandatory concurrency semantics
 
-
-/// Machine model
+/// Machine mode
+// !! exist diff calc model and libs not ever compatible
 
 
 /// Patterns
@@ -27,3 +30,46 @@
 // Non-determ.
 //   Branch and bound
 //   Transactions - without locks?! commit/rollback but how?
+
+/// Map
+// TROUBLE: how be with false sharing
+void saxpy_serial(
+    size_t n,
+    float a,
+    const float x[],
+    float y[]
+) {
+  for (size_t i = 0; i < n; ++i) {
+    y[i] = a * x[i] + y[i];
+  }
+}
+
+void saxpy_tbb(
+    size_t n,
+    float a,
+    const float x[],
+    float y[]
+) {
+  tbb::parallel_for(
+        tbb::blocked_range<
+          //int
+          size_t
+        >(0, n),  // FIXME: why int?
+        [&](tbb::blocked_range<
+          //int
+          size_t
+        > r) {
+    for (size_t i = r.begin(); i != r.end(); ++i) {
+      y[i] = a * x[i] + y[i];
+    }
+  });
+}
+
+TEST(StructConc, SAXPY) {
+  // memory intensive - not calc
+}
+
+TEST(StructConc, Mandelbrot) {
+
+}
+
