1b75528e9b017b24c00c3dd478ad60894270e411
zaqwes <igor.a.lugansky@gmail.com>
2015-05-08 12:56:26 +0300

    can't run not junit tests

---
+++ scala_projects/course_reactive/src/test/scala/AppTest.scala
@@ -0,0 +1,13 @@
+
+import org.junit._
+import Assert._
+
+// https://github.com/lihaoyi/utest
+// http://stackoverflow.com/questions/28898445/cannot-get-utest-to-see-my-tests
+
+@Test
+class AppTest {
+    @Test
+    def testOK() = assertTrue(true)
+}
+
